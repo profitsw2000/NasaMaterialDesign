f9e4051856b583ac67db1636f48d67837f716545
profitsw2000 <profitsw2000@gmail.com>
2022-02-23 11:39:57 +0300

    Создание интерфейса и класса для получения данных при помощи retrofit. Формат получаемых данных определяется вновь созданным data классом PODServerResponseData.kt.

---
+++ app/src/main/java/ru/profitsw2000/nasamaterialdesign/retrofit/PictureOfTheDayAPI.kt
@@ -0,0 +1,12 @@
+package ru.profitsw2000.nasamaterialdesign.retrofit
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+import ru.profitsw2000.nasamaterialdesign.representation.PODServerResponseData
+
+interface PictureOfTheDayAPI {
+
+    @GET("planetary/apod")
+    fun getPictureOfTheDay(@Query("api_key") apiKey: String): Call<PODServerResponseData>
+}
--- app/src/main/java/ru/profitsw2000/nasamaterialdesign/ui/fragments/MainFragment.kt
+++ app/src/main/java/ru/profitsw2000/nasamaterialdesign/ui/fragments/MainFragment.kt
@@ -8,9 +8,6 @@ import android.view.ViewGroup
 import ru.profitsw2000.nasamaterialdesign.R
 
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

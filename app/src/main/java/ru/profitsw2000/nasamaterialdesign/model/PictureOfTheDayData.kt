2417b81f9bbedc70f59ac61f3e7f7b1a450c2800
profitsw2000 <profitsw2000@gmail.com>
2022-02-23 11:46:11 +0300

    Создание sealed класса для описания результата работы view модели.

---
+++ app/src/main/java/ru/profitsw2000/nasamaterialdesign/model/PictureOfTheDayData.kt
@@ -0,0 +1,10 @@
+package ru.profitsw2000.nasamaterialdesign.model
+
+import ru.profitsw2000.nasamaterialdesign.representation.PODServerResponseData
+
+sealed class PictureOfTheDayData {
+    data class Success(val serverResponseData: PODServerResponseData) : PictureOfTheDayData()
+    data class Error(val error: Throwable) : PictureOfTheDayData()
+    data class Loading(val progress: Int?) : PictureOfTheDayData()
+}
+

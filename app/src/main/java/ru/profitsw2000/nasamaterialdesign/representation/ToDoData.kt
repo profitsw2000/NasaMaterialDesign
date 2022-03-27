151de0a3b743a415ab11170e24d0dc3bf12ad9d4
profitsw2000 <profitsw2000@gmail.com>
2022-03-27 20:01:43 +0300

    Создан макет для заголовка списка.

--- app/src/main/java/ru/profitsw2000/nasamaterialdesign/representation/ToDoData.kt
+++ app/src/main/java/ru/profitsw2000/nasamaterialdesign/representation/ToDoData.kt
@@ -5,7 +5,7 @@ import java.util.*
 const val TYPE_CLEANING = 1
 const val TYPE_LEARNING = 2
 const val TYPE_REST = 3
-const val TYPE_TITLE = 4
+const val TYPE_HEADER = 4
 
 const val TYPE_PRIORITY_VERY_LOW = 1
 const val TYPE_PRIORITY_LOW = 2
